cfc21ba5985835b6f072e62c5121d7c0ab29eb89
“PriyankaShinde” <pkshinde125@gmail.com>
2022-12-19 21:23:32 +0530

    ADDED : Testcase 1.2 for return happy mood

--- src/test/java/org/example/MoodAnalyserTest.java
+++ src/test/java/org/example/MoodAnalyserTest.java
@@ -7,9 +7,15 @@ import static org.junit.Assert.assertEquals;
 public class MoodAnalyserTest {
 
     @Test
-    public void givenMessage_whenProper_shouldReturnMood() {
+    public void givenSadMoodMessage_whenProper_shouldReturnSadMood() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
         String result = moodAnalyser.analyseMood();
         assertEquals("SAD",result);
+    }
+    @Test
+    public void givenHappyMoodMessage_whenProper_shouldReturnHappyMood() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
+        String result = moodAnalyser.analyseMood();
+        assertEquals("HAPPY",result);
     }
 }
